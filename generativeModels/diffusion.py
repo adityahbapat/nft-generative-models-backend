a992b865877785d5c88b02f8e768c2bac6be8f32
Aditya Bapat <adityahbapat@gmail.com>
2023-02-27 02:31:46 +0530

    switched to CPU

--- generativeModels/diffusion.py
+++ generativeModels/diffusion.py
@@ -8,56 +8,72 @@ import time
 
 # from keras.models import load_model
 print(os.path.exists("./generativeModels/images/diffusion"))
-print("GPU:",tf.config.list_physical_devices('GPU'))
+print("<.....GPU.....>:",tf.config.list_physical_devices('GPU'))
 print(tf.config.list_physical_devices('GPU'))
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("<......Num GPUs Available......>: ", len(tf.config.list_physical_devices('GPU')))
+
+
+# List available physical GPUs
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+
+
+# if gpus:
+#     try:
+#         for gpu in gpus:
+#         # for physical GPU configure memory limit, here I configured 4GB
+#             tf.config.experimental.set_virtual_device_configuration(gpu, [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024*4)])
+#     except RuntimeError as e:
+#         print(e)
+
+
+# if gpus:
+#     try:
+#         for gpu in gpus:
+#         # Allow TensorFlow to use shared GPU memory
+#             tf.config.experimental.set_memory_growth(gpu, True)
+#     except RuntimeError as e:
+#         print(e)
+
 
 demo_text = "photograph of an astronaut riding a horse"
 
 def getStableDiffusionModel():
-    print("entered getStableDiffusionModel")
+    print("\n<......entered getStableDiffusionModel......>\n")
     model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
-    print("exit getStableDiffusionModel")
+    print("\n<......exit getStableDiffusionModel......>\n")
     return model
 
 # model.save('diffusion_model.h5')
 # model = load_model('diffusion_model.h5')
 
 def plot_images(images):
-    print("entered plot_images")
+    print("\n<......entered plot_images......>\n")
     plt.figure(figsize=(20, 20))
     for i in range(len(images)):
         ax = plt.subplot(1, len(images), i + 1)
         plt.imshow(images[i])
         ts = time.time()
-        img_name = "image_" + str(ts) + str(i)
-        plt.savefig("./generativeModels/images/diffusion/")
-        plt.savefig("./generativeModels/images/diffusion/"+ img_name, format='png')
+        img_name = "image_" + str(ts) + str(i) + ".png"
+        plt.savefig("./generativeModels/images/diffusion/"+ img_name)
         plt.axis("off")
-    print("exit plot_images")
+    print("\n<......exit plot_images......>\n")
 
 def printImages(model):
-    print("entered printImages")
-    text = input('Enter your nft description: ')
+    print("\n<.......entered printImages.......>\n")
+    text = input('Enter your NFT description: ')
+    if text =="0":
+        exit()
+        # manually telling tf to use CPU, becaue 4GB GPU is not enough, 
+        # Ran on Google Colab and saw it took more than 12GB GPU :(
     with tf.device('/CPU:0'):
         images = model.text_to_image(text, batch_size=1)
         plot_images(images)
-    print("exit printImages")
+    print("\n<.......exit printImages.......>\n")
 
 
 keras.backend.clear_session()
 
 model = getStableDiffusionModel()
-
-# print("Model type:" + str(type(model)))
-# print(str(time.time()))
-# file = open("Python.txt", "w")
- 
-# str1 = repr(model)
-# file.write(str1)
- 
-#close file
-# file.close()
 
 for i in range(5):
     printImages(model)
